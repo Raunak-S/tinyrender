9debaeef47d16c5d0889d66e9dd4da6abf1ddfcd
Raunak S <raunak.shrestha@gmail.com>
2022-02-05 12:10:34 -0500

    Working generic implementation of Vec2D and Vec3D

--- src/geometry.rs
+++ src/geometry.rs
@@ -1,6 +1,6 @@
 use std::ops::{Add, Sub, Mul, BitXor};
 
-#[derive(Debug, Clone)]
+#[derive(Debug, Clone, Copy)]
 pub struct Vec2D<T> where T: num::Num {
     pub x: T,
     pub y: T,
@@ -33,30 +33,40 @@ impl<T> Sub for Vec2D<T> where T: num::Num {
     }
 }
 
-impl<T, F> Mul<F> for Vec2D<T> where T: num::Num
-                                    + From<F>
-                                    + num::ToPrimitive,
-                                    F: num::Float {
+impl<T> Mul<f32> for Vec2D<T> where T: num::Num
+                                        + num::NumCast
+                                        + Lossyf32 {
     type Output = Self;
 
-    fn mul(self, _rhs: F) -> Self {
+    fn mul(self, _rhs: f32) -> Self {
         Self {
-            x: T::from(F::from(self.x).unwrap()*_rhs),
-            y: T::from(F::from(self.y).unwrap()*_rhs),
+            x: T::lossy_from_f32(self.x.to_f32().unwrap()*_rhs),
+            y: T::lossy_from_f32(self.x.to_f32().unwrap()*_rhs),
         }
     }
 }
 
-#[derive(Debug, Clone)]
+#[derive(Debug, Clone, Copy)]
 pub struct Vec3D<T> where T: num::Num {
     pub x: T,
     pub y: T,
     pub z: T,
 }
 
-impl<T> Vec3D<T> where T: num::Num {
+impl<T> Vec3D<T> where T: num::Num
+                        + num::ToPrimitive
+                        + num::NumCast
+                        + Copy
+                        + Lossyf32 {
     pub fn new() -> Self { Self { x: num::zero(), y: num::zero(), z: num::zero(), } }
     pub fn new_args(newx: T, newy: T, newz: T) -> Self { Self { x: newx, y: newy, z: newz, } }
+    pub fn norm(&self) -> f32 { (self.x*self.x+self.y*self.y+self.z*self.z).to_f32().unwrap() }
+    pub fn normalize(&mut self) {
+        let inv_norm = 1.0/self.norm();
+        *self = Self { x: T::lossy_from_f32(self.x.to_f32().unwrap()*inv_norm),
+                       y: T::lossy_from_f32(self.y.to_f32().unwrap()*inv_norm),
+                       z: T::lossy_from_f32(self.z.to_f32().unwrap()*inv_norm), }; 
+    }
 }
 
 impl<T> Add for Vec3D<T> where T: num::Num {
@@ -83,22 +93,32 @@ impl<T> Sub for Vec3D<T> where T: num::Num {
     }
 }
 
-impl<T, F> Mul<F> for Vec3D<T> where T: num::Num
-                                    + From<F>
-                                    + Mul<F, Output = F>,
-                                    F: num::Float {
+impl<T> Mul<f32> for Vec3D<T> where T: num::Num
+                                    + num::NumCast
+                                    + Lossyf32 {
     type Output = Self;
 
-    fn mul(self, _rhs: F) -> Self {
+    fn mul(self, _rhs: f32) -> Self {
         Self {
-            x: T::from(self.x*_rhs),
-            y: T::from(self.y*_rhs),
-            z: T::from(self.z*_rhs),
+            x: T::lossy_from_f32(self.x.to_f32().unwrap()*_rhs),
+            y: T::lossy_from_f32(self.y.to_f32().unwrap()*_rhs),
+            z: T::lossy_from_f32(self.z.to_f32().unwrap()*_rhs),
         }
     }
 }
 
-impl<T> BitXor for Vec3D<T> where T: num::Num {
+impl<T> Mul for Vec3D<T> where T: num::Num
+                                + num::NumCast
+                                + Lossyf32 {
+    type Output = f32;
+
+    fn mul(self, rhs: Self) -> Self::Output {
+        (self.x*rhs.x + self.y*rhs.y + self.z*rhs.z).to_f32().unwrap()
+    }
+}
+
+impl<T> BitXor for Vec3D<T> where T: num::Num
+                                    + Copy {
     type Output = Self;
 
     fn bitxor(self, rhs: Self) -> Self::Output {
@@ -114,3 +134,19 @@ pub type Vec2i = Vec2D<i32>;
 pub type Vec3i = Vec3D<i32>;
 pub type Vec2f = Vec2D<f32>;
 pub type Vec3f = Vec3D<f32>;
+
+pub trait Lossyf32: Sized {
+    fn lossy_from_f32(f: f32) -> Self;
+}
+
+impl Lossyf32 for i32 {
+    fn lossy_from_f32(f: f32) -> Self {
+        f as Self
+    }
+}
+
+impl Lossyf32 for f32 {
+    fn lossy_from_f32(f: f32) -> Self {
+        f
+    }
+}
