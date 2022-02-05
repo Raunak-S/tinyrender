e41aee034573cde546b4e44ed54feaeeb054dc41
Raunak S <raunak.shrestha@gmail.com>
2022-02-05 12:10:49 -0500

    Add Lesson 2 main func

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 mod tga;
-use crate::tga::*;
+mod geometry;
+use crate::{tga::*, geometry::*};
 use obj::Obj;
 
 pub fn check_expected() {
@@ -12,13 +13,9 @@ pub fn check_expected() {
     image.write_tga_file("/mnt/c/Users/shresr/Desktop/output.tga", false).unwrap();
 }
 
-const WIDTH: i32 = 800;
-const HEIGHT: i32 = 800;
-
-fn main() {
+pub fn check_expected1() {
     let model = Obj::load("/mnt/c/Users/shresr/Desktop/african_head.obj").unwrap();
-    println!("{} {} {}", model.data.normal.len(), model.data.position.len(), model.data.objects[0].groups[0].polys.len());
-    
+
     let mut image = TGAImage::new_dimensions(WIDTH, HEIGHT, TGAFormat.RGB as i32);
     for i in 0..model.data.objects[0].groups[0].polys.len() {
         let face = &model.data.objects[0].groups[0].polys[i];
@@ -32,8 +29,37 @@ fn main() {
             line(x0, y0, x1, y1, &mut image, &WHITE);
         }
     }
+    image.flip_vertically();
+    image.write_tga_file("/mnt/c/Users/shresr/Desktop/output.tga", false).unwrap();
+}
+
+const WIDTH: i32 = 800;
+const HEIGHT: i32 = 800;
+
+fn main() {
+    let model = Obj::load("/mnt/c/Users/shresr/Desktop/african_head.obj").unwrap();
+
+    let mut image = TGAImage::new_dimensions(WIDTH, HEIGHT, TGAFormat.RGB as i32);
+    let light_dir = Vec3f::new_args(0.0,0.0,-1.0);
+    for i in 0..model.data.objects[0].groups[0].polys.len() {
+        let face = &model.data.objects[0].groups[0].polys[i];
+        let mut screen_coords = [Vec2i::new(); 3];
+        let mut world_coords = [Vec3f::new(); 3];
+        for j in 0..3 {
+            let v = model.data.position[face.0[j].0];
+            screen_coords[j] = Vec2i::new_args(((v[0]+1.)*WIDTH as f32/2.) as i32, ((v[1]+1.)*HEIGHT as f32/2.) as i32);
+            world_coords[j] = Vec3f::new_args(v[0], v[1], v[2]);
+        }
+        let mut n = (world_coords[2]-world_coords[0])^(world_coords[1]-world_coords[0]);
+        n.normalize();
+        let intensity = n*light_dir;
+        if intensity>0.0 {
+            triangle(screen_coords[0], screen_coords[1], screen_coords[2], &mut image, &TGAColor::new_rgba(intensity as u8, intensity as u8, intensity as u8, 255));
+        }
+    }
 
     image.flip_vertically();
     image.write_tga_file("/mnt/c/Users/shresr/Desktop/output.tga", false).unwrap();
+
 
 }
